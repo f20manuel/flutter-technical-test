ffda714c89a0da58ccd8ae6f078786caa962d857
Manuel Fernández <f20manuel@gmail.com>
2022-09-21 22:45:03 -0500

    ADD Auth repository

--- lib/app/modules/auth/auth_repository.dart
+++ lib/app/modules/auth/auth_repository.dart
@@ -1,4 +1,8 @@
+import 'package:fluttertest/app/data/models/user.dart';
+
 /// Auth repository
 abstract class AuthRepository {
-  
+  Stream<User?> authStateChanges();
+  Future<User> signInAnonymously();
+  Future<void> signOut();
 }
